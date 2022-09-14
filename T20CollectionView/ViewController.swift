915b75f603d57d7df7204633633c44d05cd4fe3e
aliamanvermez <maliamanvermez@gmail.com>
2022-09-14 14:20:46 +0300

    Initial Commit

---
+++ T20CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T20CollectionView
+//
+//  Created by NeonApps on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
